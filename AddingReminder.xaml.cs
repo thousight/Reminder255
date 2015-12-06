9728796b71ad2490d126deb4135a748f733d3f2b
adibajaj <adibajaj@hotmail.com>
2015-12-06 15:51:27 -0500

    Changes to GUI

--- AddingReminder.xaml.cs
+++ AddingReminder.xaml.cs
@@ -13,23 +13,18 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace Reminder255
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class AddingReminder : Page
     {
-        public MainPage()
+        public AddingReminder()
         {
             this.InitializeComponent();
-        }
-
-        private void button1_Click(System.Object sender, RoutedEventArgs e)
-        {
-
         }
     }
 }
